f4266b49d6a4b214401ec803aae3d13bcf047128
Valerii Kashpur <kashpur.v.f@gmail.com>
2024-11-25 17:39:32 +0200

    fixes for deployment

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
 def format_linter_error(error: dict) -> dict:
     return {
-    "line": error["line_number"],
-    "column": error["column_number"],
-    "message": error["text"],
-    "name": error["code"],
-    "source": "flake8"
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [format_linter_error(error) for error in errors],
+        "errors": [
+            format_linter_error(error)
+            for error in errors
+        ],
         "path": file_path,
         "status": "passed" if not errors else "failed"
     }
